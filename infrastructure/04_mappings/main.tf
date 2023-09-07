ea787306d1c383fe0a19dda417bdf2a414e3d542
Markus Mayer <widemeadows@gmail.com>
2023-09-07 19:40:36 +0200

    Add httpbin example deployment

--- infrastructure/04_mappings/main.tf
+++ infrastructure/04_mappings/main.tf
@@ -1,19 +1,19 @@
 resource "kubernetes_manifest" "linkerd-trust-anchor-cert" {
   manifest = {
     apiVersion = "cert-manager.io/v1"
-    kind = "Certificate"
-    metadata = {
-      name = "cluster-playground"
+    kind       = "Certificate"
+    metadata   = {
+      name      = "cluster-playground"
       namespace = "emissary"
     }
     spec = {
       secretName = "emissary-certs-cluster-playground"
-      issuerRef = {
+      issuerRef  = {
         name = "linkerd-self-signed-issuer"
         kind = "ClusterIssuer"
       }
       commonName = "cluster.playground"
-      dnsNames = [
+      dnsNames   = [
         "cluster-playground",
         "cluster.playground",
         "linkerd.cluster-playground",
@@ -31,16 +31,16 @@ resource "kubernetes_manifest" "linkerd-trust-anchor-cert" {
 resource "kubernetes_manifest" "emissary-listener-http" {
   manifest = {
     apiVersion = "getambassador.io/v3alpha1"
-    kind = "Listener"
-    metadata = {
-      name = "emissary-ingress-http-listener"
+    kind       = "Listener"
+    metadata   = {
+      name      = "emissary-ingress-http-listener"
       namespace = "emissary"
     }
     spec = {
-      port = 8080
-      protocol = "HTTP"
-      securityModel: "XFP"
-      l7Depth = 0
+      port        = 8080
+      protocol    = "HTTP"
+      securityModel = "XFP"
+      l7Depth     = 0
       hostBinding = {
         namespace = {
           from = "ALL"
@@ -56,16 +56,16 @@ resource "kubernetes_manifest" "emissary-listener-http" {
 resource "kubernetes_manifest" "emissary-listener-https" {
   manifest = {
     apiVersion = "getambassador.io/v3alpha1"
-    kind = "Listener"
-    metadata = {
-      name = "emissary-ingress-https-listener"
+    kind       = "Listener"
+    metadata   = {
+      name      = "emissary-ingress-https-listener"
       namespace = "emissary"
     }
     spec = {
-      port = 8443
-      protocol = "HTTPS"
-      securityModel: "XFP"
-      l7Depth = 0
+      port        = 8443
+      protocol    = "HTTPS"
+      securityModel = "XFP"
+      l7Depth     = 0
       hostBinding = {
         namespace = {
           from = "ALL"
@@ -79,13 +79,13 @@ resource "kubernetes_manifest" "emissary-listener-https" {
 resource "kubernetes_manifest" "emissary-host-catchall" {
   manifest = {
     apiVersion = "getambassador.io/v3alpha1"
-    kind = "Host"
-    metadata = {
-      name = "catchall"
+    kind       = "Host"
+    metadata   = {
+      name      = "catchall"
       namespace = "emissary"
     }
     spec = {
-      hostname = "*"
+      hostname        = "*"
       mappingSelector = {
         matchLabels = {
           host = "catchall"
@@ -97,7 +97,7 @@ resource "kubernetes_manifest" "emissary-host-catchall" {
         }
       }
       tlsSecret = {
-        name = "emissary-certs-cluster-playground"
+        name      = "emissary-certs-cluster-playground"
         namespace = "emissary"
       }
     }
@@ -108,13 +108,13 @@ resource "kubernetes_manifest" "emissary-host-catchall" {
 resource "kubernetes_manifest" "emissary-host-linkerd-http" {
   manifest = {
     apiVersion = "getambassador.io/v3alpha1"
-    kind = "Host"
-    metadata = {
-      name = "linkerd-http"
+    kind       = "Host"
+    metadata   = {
+      name      = "linkerd-http"
       namespace = "emissary"
     }
     spec = {
-      hostname = "linkerd.cluster-playground:38080" # port needs to be included
+      hostname        = "linkerd.cluster-playground:38080" # port needs to be included
       mappingSelector = {
         matchLabels = {
           host = "linkerd"
@@ -132,13 +132,13 @@ resource "kubernetes_manifest" "emissary-host-linkerd-http" {
 resource "kubernetes_manifest" "emissary-host-linkerd-https" {
   manifest = {
     apiVersion = "getambassador.io/v3alpha1"
-    kind = "Host"
-    metadata = {
-      name = "linkerd-https"
+    kind       = "Host"
+    metadata   = {
+      name      = "linkerd-https"
       namespace = "emissary"
     }
     spec = {
-      hostname = "linkerd.cluster-playground:38443" # port needs to be included
+      hostname        = "linkerd.cluster-playground:38443" # port needs to be included
       mappingSelector = {
         matchLabels = {
           host = "linkerd"
@@ -150,7 +150,7 @@ resource "kubernetes_manifest" "emissary-host-linkerd-https" {
         }
       }
       tlsSecret = {
-        name = "emissary-certs-cluster-playground"
+        name      = "emissary-certs-cluster-playground"
         namespace = "emissary"
       }
     }
@@ -163,19 +163,122 @@ resource "kubernetes_manifest" "emissary-host-linkerd-https" {
 resource "kubernetes_manifest" "linkerd-viz-mapping" {
   manifest = {
     apiVersion = "getambassador.io/v3alpha1"
-    kind = "Mapping"
-    metadata = {
-      name = "linkerd-viz"
+    kind       = "Mapping"
+    metadata   = {
+      name      = "linkerd-viz"
       namespace = "emissary"
-      labels = {
+      labels    = {
         host = "linkerd"
       }
     }
     spec = {
       # Linkerd Viz disallows public access by default; spoof localhost.
-      host_rewrite = "localhost"
-      prefix = "/"
-      service = "http://web.linkerd:8084"
+      host_rewrite  = "localhost"
+      prefix        = "/"
+      service       = "http://web.linkerd:8084"
+      allow_upgrade = [
+        "spdy/3.1",
+        "websocket"
+      ]
+    }
+  }
+}
+
+# Deploy httpbin for testing
+resource "kubernetes_deployment" "httpbin" {
+  metadata {
+    name      = "httpbin"
+    namespace = "default"
+    labels    = {
+      app = "httpbin"
+    }
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "httpbin"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "httpbin"
+        }
+      }
+
+      spec {
+        container {
+          image = "kennethreitz/httpbin"
+          name  = "httpbin"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "256Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          port {
+            container_port = 80
+            name           = "http"
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/status/200"
+              port = 80
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "httpbin" {
+  metadata {
+    name      = "httpbin"
+    namespace = "default"
+    labels    = {
+      app = "httpbin"
+    }
+  }
+  spec {
+    selector = {
+      app = "httpbin"
+    }
+    session_affinity = "ClientIP"
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
+}
+
+resource "kubernetes_manifest" "httpbin-mapping" {
+  depends_on = [kubernetes_service.httpbin]
+  manifest = {
+    apiVersion = "getambassador.io/v3alpha1"
+    kind       = "Mapping"
+    metadata   = {
+      name      = "httpbin"
+      namespace = "emissary"
+      labels    = {
+        host = "catchall"
+      }
+    }
+    spec = {
+      prefix        = "/httpbin"
+      service       = "http://httpbin.default"
       allow_upgrade = [
         "spdy/3.1",
         "websocket"
